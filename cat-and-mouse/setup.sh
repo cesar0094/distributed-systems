fa18b084e247453fd7afae95e4a71e88c10e8b7b
Cesar Rodriguez <cesar.rdz.v@gmail.com>
2015-10-12 17:11:52 +0300

    Remove mouse startup in setup.sh

--- cat-and-mouse/setup.sh
+++ cat-and-mouse/setup.sh
@@ -2,9 +2,6 @@
 
 curl -s http://www.cs.helsinki.fi/ukko/hpc-report.txt | awk '/ukko/ && $8 == "cs"' | sort -k6 -n | head -n22 | awk -F'.' '{ print $1".hpc.cs.helsinki.fi" }' | head -n5 > hosts
 
-pssh -h hosts -i -A uptime
+# parallel-ssh -h hosts -i -A uptime
 touch cmsg
-
-MOUSE_LOCATION=$(shuf -n 1 hosts)
-cat mouse.sh | parallel-ssh -H $MOUSE_LOCATION -i "mkdir cat-and-mouse; cd cat-and-mouse; cat > mouse.sh; sh mouse.sh"
 
