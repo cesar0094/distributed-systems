138e03126ffb6af3bdf88f371967b8f9c789389a
Cesar Rodriguez <cesar.rdz.v@gmail.com>
2015-10-15 16:51:32 +0300

    Modify host collection to check for ssh connection

--- cat-and-mouse/setup.sh
+++ cat-and-mouse/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -s http://www.cs.helsinki.fi/ukko/hpc-report.txt | awk '/ukko/ && $8 == "cs"' | sort -k6 -n | head -n22 | awk -F'.' '{ print $1".hpc.cs.helsinki.fi" }' | head -n5 > hosts
+curl -s http://www.cs.helsinki.fi/ukko/hpc-report.txt | awk '/ukko/ && $8 == "cs" && $4 == "yes"' | sort -k6 -n | head -n22 | awk -F'.' '{ print $1".hpc.cs.helsinki.fi" }' | head -n5 > hosts
 
 # parallel-ssh -h hosts -i -A uptime
 touch cmsg
