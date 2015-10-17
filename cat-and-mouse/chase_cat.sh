dc59216068e809740b573ec5f8469190bcc1d59c
Cesar Rodriguez <cesar.rdz.v@gmail.com>
2015-10-18 02:06:00 +0300

    Search for the mouse with the port and attack with nc

--- cat-and-mouse/chase_cat.sh
+++ cat-and-mouse/chase_cat.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 LISTY_LOCATION=$(cat listy_location)
-# HOSTNAME="localhost"
 HOSTNAME=$(hostname)
+WAIT_AFTER_ATTACK=8
 
 MY_NAME=$2
 ACTION=$1
@@ -11,25 +11,39 @@ MOUSE_PORT=$(cat nc_port_number)
 LISTY_PORT=$(cat nc_port_number)
 TIMEOUT=1
 
+function got_mouse() {
+	# got the mouse
+	echo "G $HOSTNAME $MY_NAME" | nc $LISTY_LOCATION $LISTY_PORT
+	exit
+}
+
+function attack_mouse() {
+	echo "MEOW" | nc localhost $MOUSE_PORT
+	sleep $WAIT_AFTER_ATTACK
+}
+
+function search_for_mouse() {
+
+	process_in_port=$(lsof -i :$MOUSE_PORT | grep nc | awk '{ print $2 }')
+
+	if [[ "$process_in_port" != "" ]]; then
+		echo "F $HOSTNAME $MY_NAME" | nc $LISTY_LOCATION $LISTY_PORT
+	else
+		echo "N $HOSTNAME $MY_NAME" | nc $LISTY_LOCATION $LISTY_PORT
+	fi
+
+}
+
+trap got_mouse INT
+
 # searching a node takes some time
-# sleep TASK_TIME
+# sleep $TASK_TIME
 sleep 1
 
-message=$(echo "MEOW $MY_NAME" | nc $HOSTNAME $MOUSE_PORT -w $TIMEOUT)
-
-echo "I $MY_NAME got message: '$message'"
-
-if [[ "$message" == "" ]]; then
-	# if we didn't find the mouse
-	echo "N $HOSTNAME $MY_NAME" | nc $LISTY_LOCATION $LISTY_PORT
-fi
-
-if [[ "$message" == "Gotme" ]]; then
-	# if we got the mouse
-	echo "G $HOSTNAME $MY_NAME" | nc $LISTY_LOCATION $LISTY_PORT
-fi
-
-if [[ "$message" == "Foundme" ]]; then
-	# if we found the mouse
-	echo "F $HOSTNAME $MY_NAME" | nc $LISTY_LOCATION $LISTY_PORT
+if [[ "$ACTION" == "S" ]]; then
+	search_for_mouse
+elif [[ "$ACTION" == "A" ]]; then
+	attack_mouse
+else
+	echo "Unknown action: '$ACTION'"
 fi
