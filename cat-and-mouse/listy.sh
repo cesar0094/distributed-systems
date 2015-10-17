f404542416e3673726da75a0c46bbd751b6e5eda
Cesar Rodriguez <cesar.rdz.v@gmail.com>
2015-10-18 02:06:24 +0300

    Terminate listy when receiving quit msg

--- cat-and-mouse/listy.sh
+++ cat-and-mouse/listy.sh
@@ -5,6 +5,10 @@ PORT=$(cat nc_port_number)
 while true;
 do
 	echo "Listy waiting for message"
-	nc -l $PORT >> cmsg
+	message=$(nc -l $PORT)
+	if [[ "$message" == "QUIT" ]]; then
+		exit 1
+	fi
+	echo "$message" >> cmsg
 
 done
