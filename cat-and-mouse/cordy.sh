28279001d1553cc08a699a9126ef0a1c409cce0f
Cesar Rodriguez <cesar.rdz.v@gmail.com>
2015-10-18 16:37:08 +0300

    Fix array access

--- cat-and-mouse/cordy.sh
+++ cat-and-mouse/cordy.sh
@@ -60,7 +60,7 @@ function explore_next_host() {
 	lockfile $NEXT_HOST_LOCK
 
 	next_host=$(cat $NEXT_HOST_FILE)
-	another_host=$host_list[$((next_host))]
+	another_host=${host_list[$((next_host))]}
 	next_host=$((next_host+1))
 	echo "$next_host" > $NEXT_HOST_FILE
 
